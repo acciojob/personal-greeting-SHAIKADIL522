54f41a2adf1818a5a6c7d398b4ae47788f3f1271
ADIL SHAIK <adilshaik9112@gmail.com>
2026-04-01 12:40:58 +0530

    Add personalized greeting component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-
-import React from "react";
+import { useState } from 'react';
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState('');
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <input
+        type="text"
+        value={name}
+        onChange={handleChange}
+        placeholder="Enter your name"
+      />
+      {name && <p>Hello, {name}!</p>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
